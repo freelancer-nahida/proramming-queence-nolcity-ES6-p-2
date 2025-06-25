cd4b25b5a481bbb28df7f92fb7191b7e2488a0fb
freelancer-nahida <freelancernahidasultana@gmail.com>
2025-06-25 17:30:29 +0600

    add temp.js

---
+++ temp.js
@@ -0,0 +1,13 @@
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+
+// }
+// add(2, 6);
+
+let num = 6;
+
+let num3 = 2;
+
+const massage = `${num} and ${num3} equal to ${num + num3}`;
+
+console.log(massage);
