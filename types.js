9759b6d43d4e837917a020fd91b7c8d07402536c
freelancer-nahida <freelancernahidasultana@gmail.com>
2025-06-26 20:29:50 +0600

    add types.js

---
+++ types.js
@@ -0,0 +1,50 @@
+
+
+// int a = 5;
+
+//string b = 'hello';
+
+//bool c = true;
+
+//object student = { name: 'sumaiya', id : 55};
+
+// string[] friend = ['abul', 'babul']
+
+// int[ ] numbers = [32, 44 , 42];
+
+// dynamic type programming
+
+
+// primitive type
+// const a = 5;
+// const b = 'hell0';
+// const c = true;
+
+// non-primitive Type
+// const student = { name: 'sumaiya', id: 55 };
+
+// const friend = ['abul', 'babul'];
+
+// const numbers = [2, 3, 5, 6];
+
+// console.log(typeof a, typeof b, typeof c, typeof student, typeof friend, typeof numbers);
+
+// let x = 5;
+
+// let y = x;
+
+// console.log(x, y);
+
+// y = 7;
+
+// console.log(x, y);
+
+let p = { job: 'web Dev' };
+
+let q = p;
+// q = p;
+q.jop = 'front End Dev';
+// q = { job: 'back Dev' };
+console.log(p, q);
+
+
